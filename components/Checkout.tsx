bdbc10dddd6a382e8bbf305f42bf6ec0d33e6148
BisiDavi <oludavidconnect@gmail.com>
2023-01-29 19:39:28 +0100

    checkout updated

--- components/Checkout.tsx
+++ components/Checkout.tsx
@@ -1,4 +1,5 @@
 import useCartContext from "@/hooks/useCartContext";
+import Trash from "@/public/Trash";
 import Image from "next/image";
 
 export default function Checkout() {
@@ -7,7 +8,9 @@ export default function Checkout() {
   return (
     <>
       <div className="checkout">
-        <form></form>
+        <form>
+          <button>Checkout</button>
+        </form>
         <ul className="cart">
           {cart.map((item) => (
             <li key={item.title}>
@@ -20,8 +23,18 @@ export default function Checkout() {
               <div className="text-content">
                 <h4>{item.title}</h4>
                 <h5>
-                  {item.quantity} X {item.price} = {item.quantity * item.price}
+                  {item.price} X {item.quantity} = ${item.quantity * item.price}
                 </h5>
+                <div className="control-group">
+                  <div className="controls">
+                    <button className="add">+</button>
+                    <span>{item.quantity}</span>
+                    <button className="minus">-</button>
+                  </div>
+                  <button>
+                    <Trash />
+                  </button>
+                </div>
               </div>
             </li>
           ))}
@@ -37,11 +50,84 @@ export default function Checkout() {
             display: flex;
             flex-direction: column;
             padding: 0px;
+            width: 100%;
           }
 
           .cart li {
             display: flex;
-            margin: 10px 0px;
+            margin: 0px 0px;
+            border-bottom: 1px solid #a6a1a1a8;
+            padding-bottom: 10px;
+            align-items: center;
+            width: 100%;
+            padding: 10px;
+          }
+
+          .cart li:hover {
+            background-color: #d6d1d157;
+          }
+
+          .cart li .text-content {
+            margin-left: 20px;
+            width: 100%;
+          }
+
+          .cart .text-content h4 {
+            font-size: 20px;
+            margin: 0px;
+          }
+
+          .cart .text-content h5 {
+            font-size: 18px;
+            margin: 8px 0px;
+          }
+
+          .control-group {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            width: 100%;
+          }
+
+          .control-group button {
+            border: none;
+            border-radius: 5px;
+            padding: 5px;
+            cursor: pointer;
+          }
+
+          .controls {
+            display: flex;
+            width: 30%;
+            justify-content: space-between;
+            align-items: center;
+          }
+
+          .controls span {
+            font-size: 25px;
+            font-weight: bold;
+          }
+
+          .controls button {
+            font-size: 40px;
+            height: 40px;
+            width: 40px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            color: white;
+          }
+
+          .controls .add {
+            background-color: green;
+          }
+
+          .controls .minus {
+            background-color: red;
+          }
+
+          .controls button:hover {
+            opacity: 0.8;
           }
         `}
       </style>
