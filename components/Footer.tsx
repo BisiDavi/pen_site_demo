4905cbd3373ccb66bb4972ba5e17b2993f97f75f
BisiDavi <oludavidconnect@gmail.com>
2023-01-29 16:11:37 +0100

    footer  updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,21 @@
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <>
+      <footer className="footer">
+        <p>© 2023, GetthisGreen demo, built with ❤️ & 🎧</p>
+      </footer>
+      <style jsx>
+        {`
+          .footer {
+            background-color: green;
+            color: white;
+            height: 40px;
+            padding: 20px 40px;
+            font-style: italic;
+            font-family: "Roboto Mono", sans-serif;
+          }
+        `}
+      </style>
+    </>
+  );
 }
