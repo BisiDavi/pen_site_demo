0c4b200dceeba03243ae59a011b0adf9834877ba
BisiDavi <oludavidconnect@gmail.com>
2023-01-30 03:22:28 +0100

    form component  added

--- pages/api/get-express-rate.ts
+++ pages/api/get-express-rate.ts
@@ -3,9 +3,41 @@ import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const {
+    originCountryCode,
+    originCityName,
+    destinationCountryCode,
+    destinationCityName,
+    weight,
+    length,
+    width,
+    height,
+    isCustomsDeclarable,
+    unitOfMeasurement,
+    plannedShippingDate,
+  } = req.body;
   switch (req.method) {
     case "POST": {
-      axios.get("");
+      axios.get(`${process.env.DHL_BASIC_URL}/rates`, {
+        params: {
+          accountNumber: `${process.env.DHL_ACCOUNTNUMBER}`,
+          originCountryCode,
+          originCityName,
+          destinationCountryCode,
+          destinationCityName,
+          weight,
+          length,
+          width,
+          height,
+          isCustomsDeclarable,
+          unitOfMeasurement,
+          plannedShippingDate,
+        },
+        auth: {
+          username: `${process.env.DHL_USERNAME}`,
+          password: `${process.env.DHL_PASSWORD}`,
+        },
+      });
       res.status(200).json({ name: "John Doe" });
     }
   }
